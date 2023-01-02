17161be7ce043f6d36ca2c50137db743550967e7
zhangpanrobot <zhangpanrobot@gmail.com>
2023-01-03 02:24:14 +0800

    92

--- cpp/solution/s92_reverse_linked_list_ii.cpp
+++ cpp/solution/s92_reverse_linked_list_ii.cpp
@@ -4,6 +4,14 @@
  * [92] Reverse Linked List II
  */
 
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
 // @lc code=start
 /**
  * Definition for singly-linked list.
@@ -15,11 +23,37 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
-public:
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        
+ public:
+  ListNode* reverseBetween(ListNode* head, int left, int right) {
+    if (head == nullptr) return head;
+    int curIndex = 1;
+    ListNode* reserveNode = head;
+    ListNode* curNode = head;
+    ListNode* prevNode = head;
+    ListNode* leftNode;
+
+    while (curIndex <= right && curNode != nullptr) {
+      if (curIndex == left - 1) {
+        reserveNode = prevNode->next;
+        leftNode = curNode->next;
+      }
+      ListNode* temp = curNode->next;
+      if (curIndex > left && temp != nullptr) {
+        curNode->next = prevNode;
+        prevNode = curNode;
+        curNode = temp;
+      }
+      if (curIndex == right) {
+        reserveNode->next = curNode;
+        leftNode->next = curNode->next;
+      }
+      if (temp) {
+        curIndex++;
+      }
     }
+    return head;
+  }
 };
 // @lc code=end
-
